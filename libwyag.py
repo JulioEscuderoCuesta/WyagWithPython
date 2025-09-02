12db63212262d20c76b03b060c10e7ed4574a838
jescudero <jescudero@kunak.es>
2025-09-02 16:46:24 +0200

    wyag to python file to execute in windows. Function cmd_init done. Init command works

--- libwyag.py
+++ libwyag.py
@@ -1,10 +1,9 @@
 import argparse
 import configparser
 from datetime import datetime
-import grp, pwd
-from fnmath import fnmatch
-import haslib
-fromt math import ceil
+from fnmatch import fnmatch
+import hashlib
+from math import ceil
 import os
 import re
 import sys
@@ -15,107 +14,109 @@ import zlib
 ############################################
 
 ## 3.1. The Repository object
+# Main parser
 argparser = argparse.ArgumentParser(description="description")
 
+# Subparser. It gets the command alongside wyag
 argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
 argsubparsers.required = True
 
 def main(argv=sys.argv[1:]):
-	args = argparser.parse_args(argv)
-	match args.command:
-		case "add" : cmd_add(args)
-        case "cat-file" : cmd_cat_file(args)
-        case "check-ignore" : cmd_check_ignore(args)
-        case "checkout" : cmd_checkout(args)
-        case "commit" : cmd_commit(args)
-        case "hash-object" : cmd_hash_object(args)
-        case "init" : cmd_init(args)
-        case "log" : cmd_log(args)
-        case "ls-files" : cmd_ls_files(args)
-        case "ls-tree" : cmd_ls_tree(args)
-        case "rev-parse" : cmd_rev_parse(args)
-        case "rm" : cmd_rm(args)
-        case "show-ref" : cmd_show_ref(args)
-        case "status" : cmd_status(args)
-        case "tag" : cmd_tag(args)
-		case _ : print("Bad command.")	
+    args = argparser.parse_args(argv)
+    match args.command:
+        case "add": cmd_add(args)
+        case "cat-file": cmd_cat_file(args)
+        case "check-ignore": cmd_check_ignore(args)
+        case "checkout": cmd_checkout(args)
+        case "commit": cmd_commit(args)
+        case "hash-object": cmd_hash_object(args)
+        case "init": cmd_init(args)
+        case "log": cmd_log(args)
+        case "ls-files": cmd_ls_files(args)
+        case "ls-tree": cmd_ls_tree(args)
+        case "rev-parse": cmd_rev_parse(args)
+        case "rm": cmd_rm(args)
+        case "show-ref": cmd_show_ref(args)
+        case "status": cmd_status(args)
+        case "tag": cmd_tag(args)
+        case _: print("Bad command.")
 
 
 # Repository class. Creates a basic repository. Saves working tree, gitdir (within working tree) and configuration
 class GitRepository (object):
-	"""A git repository"""
+    """A git repository"""
 
-	worktree = None
-	gitdir = None
-	conf = None
+    worktree = None
+    gitdir = None
+    conf = None
 
-	def __init__(self, path, force=False):
-		self.worktree = path
-		self.gitdir = os.path.join(path, ".git")
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
 
-		if not (force or os.path.isdir(self.gitdir)):
-			raise Exception(f"Not a Git repository {path}")
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception(f"Not a Git repository {path}")
 
-		# Read configuration file in .git/config
-		self.conf = configparser.ConfigParser()
-		cf = repo_file(self, "config")
+        # Read configuration file in .git/config
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
 
-		if cf and os.path.exists(cf):
-			self.conf.read([cf])
-		elif not force:
-			raise Exception("Configuration file missing")
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
 
-		if not force:
-			vers = int(self.conf.get("core", "repositoryformatversion"))
-			if vers != 0:
-				raise Exception(f"Unsupported repositoryformatversion: {vers}")
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception(f"Unsupported repositoryformatversion: {vers}")
 
 
 # Creates and returns path under a given path.
 def repo_path(repo, *path):
-	"""Compute path under repo's gitdir."""
-	return os.path.join(repo.gitdir, *path)
+    """Compute path under repo's gitdir."""
+    return os.path.join(repo.gitdir, *path)
 
 
 # Returns and optionally create a path to a file
 def repo_file(repo, *path, mkdir=False):
     """Same as repo_path, but create dirname(*path) if absent.  For
-example, repo_file(r, \"refs\", \"remotes\", \"origin\", \"HEAD\") will create
-.git/refs/remotes/origin."""
-	
-	if repo_dir(repo, *path[:-1], mkdir=mkdir):
-		return repo_path(repo, *path)
+    example, repo_file(r, \"refs\", \"remotes\", \"origin\", \"HEAD\") will create
+    .git/refs/remotes/origin."""
+
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
 
 
 #Returns and optionally create a path to a directory.
 def repo_dir(repo, *path, mkdir=False):
-	"""Same as repo_path, but mkdir *path if absent if mkdir."""
+    """Same as repo_path, but mkdir *path if absent if mkdir."""
 
-	path = repo_path(repo, *path)
+    path = repo_path(repo, *path)
 
-	if os.path.exists(path):
-		if (os.path.isdir(path)):
-			return path
-		else:
-			raise Exception(f"Not a directory {path}")
+    if os.path.exists(path):
+        if (os.path.isdir(path)):
+            return path
+        else:
+            raise Exception(f"Not a directory {path}")
 
-	if mkdir:
-		os.makedirs(path)
-		return path
-	else:
-		return None
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
 
 
 # Creates simple configuration file 
 def repo_default_config():
-	ret = configparser.ConfigParser()
+    ret = configparser.ConfigParser()
 
-	ret.add_section("core")
-	ret.set("core", "repositoryformatversion", "0")
-	ret.set("core", "filemode", "false")
-	ret.set("core", "bare", "false")
+    ret.add_section("core")
+    ret.set("core", "repositoryformatversion", "0")
+    ret.set("core", "filemode", "false")
+    ret.set("core", "bare", "false")
 
-	return ret
+    return ret
 
 
 # Creates new repository at a path. 
@@ -123,36 +124,52 @@ def repo_default_config():
 # Creates files with default syntax
 # If repository already exists within the path and contains something, raise exception
 def repo_create(path):
-	repo = GitRepository(path, True)
+    repo = GitRepository(path, True)
 
-	# First, we make sure the path either doesn't exist or is an empty dir.
+    # First, we make sure the path either doesn't exist or is an empty dir.
 
-	if os.path.exists(repo.worktree):
-		if not os.path.isdir(repo.worktree):
-			raise Exception (f"{path} is not a directory!")
-		if os.path.exists(repo.gitdir) and os.listdir(repo.gitdir):
-			raise Exception (f"{path} is not empty!")
-	else:
-		os.makedirs(repo.worktree)
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception (f"{path} is not a directory!")
+        if os.path.exists(repo.gitdir) and os.listdir(repo.gitdir):
+            raise Exception (f"{path} is not empty!")
+    else:
+        os.makedirs(repo.worktree)
 
-	assert repo_dir(repo, "branches", mkdir=True)
-	assert repo_dir(repo, "objects", mkdir=True)
-	assert repo_dir(repo, "refs", "tags", mkdir=True)
-	assert repo_dir(repo, "refs", "heads", mkdir=True)
+    assert repo_dir(repo, "branches", mkdir=True)
+    assert repo_dir(repo, "objects", mkdir=True)
+    assert repo_dir(repo, "refs", "tags", mkdir=True)
+    assert repo_dir(repo, "refs", "heads", mkdir=True)
 
-	# .git/description
-	with open(repo_file(repo, "description"), "w") as f:
-		f.write("Unnamed repository; edit this file 'description' to name the repository. \n")
+    # .git/description
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write("Unnamed repository; edit this file 'description' to name the repository. \n")
 
-	# .git/HEAD
-	with open(repo_file(repo, "HEAD"), "w") as f:
-		f.write("ref: refs/heads/master\n")
+    # .git/HEAD
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
 
-	with open(repo_file(repo, "config"), "w") as f:
-		config = repo_default_config()
-		config.write(f)
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
 
-	return repo
+    return repo
 
 
 ## 3.2. The init command
+
+# Argparse subparser to handle command's argument
+argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository")
+
+# Specific subparser. Saves path provided into "args.path". "." by deafult 
+argsp.add_argument("path",
+                    metavar="directory",
+                    nargs="?",
+                    default=".",
+                    help="Whre to create the repository.")
+
+# Init function. Gets arguments through command line and call repo_create with the path
+def cmd_init(args):
+    repo_create(args.path)
+
+
